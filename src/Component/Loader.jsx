db87444fa7967ff98895648a4d082b7a8076ebe7
Fagbelu Stephen <fagbelustephen@gmail.com>
2024-09-26 19:47:16 +0100

    fix

--- src/Component/Loader.jsx
+++ src/Component/Loader.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Loader = () => {
   return (
     <span className='flex items-center justify-center w-[10rem] h-[10rem]'>
-      <img src="public/Pr.png" alt="Loader" className='w-[10rem] animate-pulse'/>
+      <img src="/Pr.png" alt="Loader" className='w-[10rem] animate-pulse'/>
     </span>
   )
 }
